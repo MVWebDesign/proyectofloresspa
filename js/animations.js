a35f76dfe5802dc16406a20f2d4a84d0ad572813
MVWebDesign <95200428+MVWebDesign@users.noreply.github.com>
2022-05-17 00:36:27 -0500

    Add files via upload

---
+++ js/animations.js
@@ -0,0 +1,24 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.animated', {
+        duration: 1500,
+        origin: 'top',
+        distance: '-80px',
+        reset: true
+    });
+
+    sr.reveal('.animated2', {
+        duration: 1500,
+        origin: 'left',
+        delay: 100,
+        distance: '50px',
+        reset: true
+    });
+
+    sr.reveal('.animated3', {
+        duration: 1500,
+        origin: 'right',
+        delay: 100,
+        distance: '50px',
+        reset: true
+    });
